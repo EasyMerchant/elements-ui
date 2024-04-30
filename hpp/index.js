dc749a2be8a818aa074ff9289787399057eefe28
Akshay Balwani <akshay.balwani@acquaintsof.com>
2024-04-30 19:56:47 +0530

    HPP Page Script and Responsiveness

--- hpp/index.js
+++ hpp/index.js
@@ -1,6 +1,19 @@
 (function () {
   const steps = document.querySelectorAll(".step");
   const allFields = document.querySelectorAll(".fields");
+  const continueBtn = document.querySelector("#continue-btn");
+  const payNowBtn = document.querySelector("#pay-now");
+  const payLaterBtn = document.querySelector("#pay-later");
+  const payBtn = document.querySelector("#pay-btn");
+  const billingCheckbox = document.querySelector("#billing-info");
+  const addressField = document.querySelector("#address-details");
+  const cardOnNameField = document.querySelector("#card-name");
+  const mobileSummary = document.querySelector("#mobile-summary-card");
+  const paySection = document.querySelector("#pay-section");
+  const reviewSection = document.querySelector("#review-section");
+  const header = document.querySelector("#header");
+  const doneSection = document.querySelector("#done-section");
+  const loader = document.querySelector("#loader");
 
   allFields[0].style.display = "flex";
 
@@ -41,5 +54,48 @@
       removeActive(index);
       renderFields(el.dataset.name);
     });
+  });
+
+  continueBtn.addEventListener("click", (e) => {
+    e.preventDefault();
+    removeActive(1);
+    renderFields("payment");
+  });
+
+  payNowBtn.addEventListener("click", (e) => {
+    e.preventDefault();
+    removeActive(2);
+    reviewSection?.classList?.remove("hidden");
+    paySection?.classList?.add("hidden");
+    mobileSummary?.classList?.add("hidden");
+    console.log("Paynow clicked");
+  });
+
+  payLaterBtn.addEventListener("click", (e) => {
+    e.preventDefault();
+    console.log("Paylater clicked");
+  });
+
+  payBtn.addEventListener("click", (e) => {
+    e.preventDefault();
+    reviewSection?.classList?.add("hidden");
+    loader?.classList?.remove("hidden");
+    header?.classList?.add("hidden");
+    setTimeout(() => {
+      loader?.classList?.add("hidden");
+      doneSection?.classList?.remove("hidden");
+    }, 3000);
+    console.log("paybtn clicked");
+  });
+
+  billingCheckbox.addEventListener("change", (e) => {
+    const value = e.target?.checked;
+    if (value) {
+      cardOnNameField.style.display = "none";
+      addressField.style.display = "none";
+    } else {
+      cardOnNameField.style.display = "flex";
+      addressField.style.display = "flex";
+    }
   });
 })();
